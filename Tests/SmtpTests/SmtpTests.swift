07b6bd6c0818b67308dd50bd0141a514ece350b6
Marcin Czachurski <mczachurski@icloud.com>
2020-03-31 09:30:04 +0200

    Add unit tests to Github actions

--- Tests/SmtpTests/SmtpTests.swift
+++ Tests/SmtpTests/SmtpTests.swift
@@ -7,20 +7,23 @@ final class SmtpTests: XCTestCase {
 
     let smtpConfiguration = SmtpServerConfiguration(hostname: "smtp.mailtrap.io",
                                                     port: 465,
-                                                    username: "",
-                                                    password: "")
+                                                    username: "8396cb1ecc7959",
+                                                    password: "#MAILTRAPPASS#",
+                                                    secure: .none)
 
     let sslSmtpConfiguration = SmtpServerConfiguration(hostname: "smtp.gmail.com",
                                                        port: 465,
-                                                       username: "",
-                                                       password: "",
+                                                       username: "smtp.mikroservice@gmail.com",
+                                                       password: "#GMAILPASS#",
                                                        secure: .ssl)
 
     let tslSmtpConfiguration = SmtpServerConfiguration(hostname: "smtp.gmail.com",
                                                        port: 587,
-                                                       username: "",
-                                                       password: "",
+                                                       username: "smtp.mikroservice@gmail.com",
+                                                       password: "#GMAILPASS#",
                                                        secure: .startTls)
+
+    let timestamp = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short)
 
     func testSendTextMessage() throws {
         let application = Application()
@@ -31,7 +34,7 @@ final class SmtpTests: XCTestCase {
         application.smtp.configuration = smtpConfiguration
         let email = Email(from: EmailAddress(address: "john.doe@testxx.com", name: "John Doe"),
                           to: [EmailAddress(address: "ben.doe@testxx.com", name: "Ben Doe")],
-                          subject: "The subject (text)",
+                          subject: "The subject (text) - \(timestamp)",
                           body: "This is email body.")
         
         let request = Request(application: application, on: application.eventLoopGroup.next())
@@ -51,7 +54,7 @@ final class SmtpTests: XCTestCase {
         application.smtp.configuration = smtpConfiguration
         let email = Email(from: EmailAddress(address: "john.doe@testxx.com"),
                           to: [EmailAddress(address: "ben.doe@testxx.com")],
-                          subject: "The subject (without names)",
+                          subject: "The subject (without names) - \(timestamp)",
                           body: "This is email body.")
 
         let request = Request(application: application, on: application.eventLoopGroup.next())
@@ -71,7 +74,7 @@ final class SmtpTests: XCTestCase {
         application.smtp.configuration = smtpConfiguration
         let email = Email(from: EmailAddress(address: "john.doe@testxx.com", name: "John Doe"),
                           to: [EmailAddress(address: "ben.doe@testxx.com", name: "Ben Doe")],
-                          subject: "The subject (html)",
+                          subject: "The subject (html) - \(timestamp)",
                           body: "<html><body><h1>This is email content!</h1></body></html>",
                           isBodyHtml: true)
 
@@ -92,7 +95,7 @@ final class SmtpTests: XCTestCase {
         application.smtp.configuration = smtpConfiguration
         var email = Email(from: EmailAddress(address: "john.doe@testxx.com", name: "John Doe"),
                           to: [EmailAddress(address: "ben.doe@testxx.com", name: "Ben Doe")],
-                          subject: "The subject (text)",
+                          subject: "The subject (text) - \(timestamp)",
                           body: "This is email body.")
 
         email.addAttachment(Attachment(name: "plik1.txt", contentType: "text/plain", data: Attachments.text()))
@@ -115,55 +118,9 @@ final class SmtpTests: XCTestCase {
         application.smtp.configuration = smtpConfiguration
         var email = Email(from: EmailAddress(address: "john.doe@testxx.com", name: "John Doe"),
                           to: [EmailAddress(address: "ben.doe@testxx.com", name: "Ben Doe")],
-                          subject: "The subject (html)",
+                          subject: "The subject (html) - \(timestamp)",
                           body: "<html><body><h1>This is email content!</h1></body></html>",
                           isBodyHtml: true)
-
-        email.addAttachment(Attachment(name: "plik1.txt", contentType: "text/plain", data: Attachments.text()))
-        email.addAttachment(Attachment(name: "image.png", contentType: "image/png", data: Attachments.image()))
-
-        let request = Request(application: application, on: application.eventLoopGroup.next())
-        try request.send(email) { message in
-            print(message)
-        }.flatMapThrowing { result in
-            XCTAssertTrue(try result.get())
-        }.wait()
-    }
-
-    func testSendTextMessageOverSSL() throws {
-        let application = Application()
-        defer {
-            application.shutdown()
-        }
-        
-        application.smtp.configuration = sslSmtpConfiguration
-        var email = Email(from: EmailAddress(address: "marcincz@gmail.com", name: "John Doe"),
-                          to: [EmailAddress(address: "mczachurski@icloud.com", name: "Ben Doe")],
-                          subject: "The subject (text)",
-                          body: "This is email body.")
-
-        email.addAttachment(Attachment(name: "plik1.txt", contentType: "text/plain", data: Attachments.text()))
-        email.addAttachment(Attachment(name: "image.png", contentType: "image/png", data: Attachments.image()))
-
-        let request = Request(application: application, on: application.eventLoopGroup.next())
-        try request.send(email) { message in
-            print(message)
-        }.flatMapThrowing { result in
-            XCTAssertTrue(try result.get())
-        }.wait()
-    }
-
-    func testSendTextMessageOverTSL() throws {
-        let application = Application()
-        defer {
-            application.shutdown()
-        }
-
-        application.smtp.configuration = tslSmtpConfiguration
-        var email = Email(from: EmailAddress(address: "marcincz@gmail.com", name: "John Doe"),
-                          to: [EmailAddress(address: "mczachurski@icloud.com", name: "Ben Doe")],
-                          subject: "The subject (text)",
-                          body: "This is email body.")
 
         email.addAttachment(Attachment(name: "plik1.txt", contentType: "text/plain", data: Attachments.text()))
         email.addAttachment(Attachment(name: "image.png", contentType: "image/png", data: Attachments.image()))
@@ -188,7 +145,7 @@ final class SmtpTests: XCTestCase {
                             EmailAddress(address: "ben.doe@testxx.com", name: "Ben Doe"),
                             EmailAddress(address: "anton.doe@testxx.com", name: "Anton Doe")
                           ],
-                          subject: "The subject (multiple to)",
+                          subject: "The subject (multiple to) - \(timestamp)",
                           body: "This is email body.")
 
         let request = Request(application: application, on: application.eventLoopGroup.next())
@@ -215,7 +172,7 @@ final class SmtpTests: XCTestCase {
                             EmailAddress(address: "tom.doe@testxx.com", name: "Tom Doe"),
                             EmailAddress(address: "rob.doe@testxx.com", name: "Rob Doe")
                           ],
-                          subject: "The subject (multiple cc)",
+                          subject: "The subject (multiple cc) - \(timestamp)",
                           body: "This is email body.")
 
         let request = Request(application: application, on: application.eventLoopGroup.next())
@@ -235,9 +192,55 @@ final class SmtpTests: XCTestCase {
         application.smtp.configuration = smtpConfiguration
         let email = Email(from: EmailAddress(address: "john.doe@testxx.com", name: "John Doe"),
                           to: [EmailAddress(address: "ben.doe@testxx.com", name: "Ben Doe")],
-                          subject: "The subject (reply-to)",
+                          subject: "The subject (reply-to) - \(timestamp)",
                           body: "This is email body.",
                           replyTo: EmailAddress(address: "noreply@testxx.com"))
+
+        let request = Request(application: application, on: application.eventLoopGroup.next())
+        try request.send(email) { message in
+            print(message)
+        }.flatMapThrowing { result in
+            XCTAssertTrue(try result.get())
+        }.wait()
+    }
+
+    func testSendTextMessageOverSSL() throws {
+        let application = Application()
+        defer {
+            application.shutdown()
+        }
+        
+        application.smtp.configuration = sslSmtpConfiguration
+        var email = Email(from: EmailAddress(address: "smtp.mikroservice@gmail.com", name: "John Doe"),
+                          to: [EmailAddress(address: "smtp.mikroservice@outlook.com", name: "Ben Doe")],
+                          subject: "The subject (over SSL) - \(timestamp)",
+                          body: "This is email body.")
+
+        email.addAttachment(Attachment(name: "plik1.txt", contentType: "text/plain", data: Attachments.text()))
+        email.addAttachment(Attachment(name: "image.png", contentType: "image/png", data: Attachments.image()))
+
+        let request = Request(application: application, on: application.eventLoopGroup.next())
+        try request.send(email) { message in
+            print(message)
+        }.flatMapThrowing { result in
+            XCTAssertTrue(try result.get())
+        }.wait()
+    }
+
+    func testSendTextMessageOverTSL() throws {
+        let application = Application()
+        defer {
+            application.shutdown()
+        }
+
+        application.smtp.configuration = tslSmtpConfiguration
+        var email = Email(from: EmailAddress(address: "smtp.mikroservice@gmail.com", name: "John Doe"),
+                          to: [EmailAddress(address: "smtp.mikroservice@outlook.com", name: "Ben Doe")],
+                          subject: "The subject (over TSL) - \(timestamp)",
+                          body: "This is email body.")
+
+        email.addAttachment(Attachment(name: "plik1.txt", contentType: "text/plain", data: Attachments.text()))
+        email.addAttachment(Attachment(name: "image.png", contentType: "image/png", data: Attachments.image()))
 
         let request = Request(application: application, on: application.eventLoopGroup.next())
         try request.send(email) { message in
@@ -253,10 +256,10 @@ final class SmtpTests: XCTestCase {
         ("testSendTextMessageWithoutNames", testSendTextMessageWithoutNames),
         ("testSendTextMessageWithAttachments", testSendTextMessageWithAttachments),
         ("testSendHtmlMessageWithAttachments", testSendHtmlMessageWithAttachments),
-        ("testSendTextMessageOverSSL", testSendTextMessageOverSSL),
-        ("testSendTextMessageOverTSL", testSendTextMessageOverTSL),
         ("testSendTextMessageToMultipleRecipients", testSendTextMessageToMultipleRecipients),
         ("testSendTextMessageWithCC", testSendTextMessageWithCC),
-        ("testSendTextMessageWithReplyTo", testSendTextMessageWithReplyTo)
+        ("testSendTextMessageWithReplyTo", testSendTextMessageWithReplyTo),
+        ("testSendTextMessageOverSSL", testSendTextMessageOverSSL),
+        ("testSendTextMessageOverTSL", testSendTextMessageOverTSL)
     ]
 }
